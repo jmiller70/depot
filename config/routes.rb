197b8baab2c7c92958fab4632f44876e67d8f43f
jmiller70 <jmillersiu@gmail.com>
2015-02-13 12:24:16 -0600

    admin1

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 #---
 # Excerpted from "Agile Web Development with Rails",
 # published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
+# Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
+# We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 Depot::Application.routes.draw do
-  get 'admin/index'
-
-  get 'sessions/new'
+  get 'admin' => 'admin#index'
+    controller :sessions do
+      get  'login' => :new
+      post 'login' => :create
+      delete 'logout' => :destroy
+    end
+  
 
   get 'sessions/create'
 
@@ -31,5 +35,5 @@ Depot::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
   root 'store#index', as: 'store'
-  
+
 end
