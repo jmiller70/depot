52a065e51ba9afef038ea85e35e9bddeac2a33c4
Jeff Miller <jmiller@siu.edu>
2015-02-13 17:38:58 -0600

    by

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-config.action_mailer.delivery_method = :test
+#config.action_mailer.delivery_method = :test
 # Initialize the Rails application.
 Depot::Application.initialize!
