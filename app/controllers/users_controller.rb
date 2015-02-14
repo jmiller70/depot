21e343620a045bb74d9956df0033d2a888d05c69
Jeff Miller <jmiller@siu.edu>
2015-02-13 22:40:33 -0600

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.order(:name)
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+end
   end
 
   # GET /users/1
