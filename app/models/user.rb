a763558c1e6615539ca6b312bb54e6c7592328b6
Jeff Miller <jmiller@siu.edu>
2016-04-22 08:14:43 -0500

    dbschema.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
-  has_secure_password
+  #validates :name, presence: true, uniqueness: true
+  #has_secure_password
 
   after_destroy :ensure_an_admin_remains
 
